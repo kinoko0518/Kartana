d21b54723364979418f1cf4fa4dab4d3b467da57
6osciola <99010199+kinoko0518@users.noreply.github.com>
2025-12-30 17:14:29 +0900

    Splitted main.css

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,14 @@ enum Route {
 }
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
-const MAIN_CSS: Asset = asset!("/assets/main.css");
+const VARIABLES_CSS: Asset = asset!("/assets/css/variables.css");
+const BASE_CSS: Asset = asset!("/assets/css/base.css");
+const LAYOUT_CSS: Asset = asset!("/assets/css/layout.css");
+const CARDS_CSS: Asset = asset!("/assets/css/cards.css");
+const CHAPTER_LIST_CSS: Asset = asset!("/assets/css/chapter_list.css");
+const FORMS_CSS: Asset = asset!("/assets/css/forms.css");
+const ICONS_CSS: Asset = asset!("/assets/css/icons.css");
+const MODAL_CSS: Asset = asset!("/assets/css/modal.css");
 
 fn main() {
     dioxus::launch(App);
@@ -21,7 +28,14 @@ fn main() {
 fn App() -> Element {
     rsx! {
         document::Link { rel: "icon", href: FAVICON }
-        document::Link { rel: "stylesheet", href: MAIN_CSS }
+        document::Link { rel: "stylesheet", href: VARIABLES_CSS }
+        document::Link { rel: "stylesheet", href: BASE_CSS }
+        document::Link { rel: "stylesheet", href: LAYOUT_CSS }
+        document::Link { rel: "stylesheet", href: CARDS_CSS }
+        document::Link { rel: "stylesheet", href: CHAPTER_LIST_CSS }
+        document::Link { rel: "stylesheet", href: FORMS_CSS }
+        document::Link { rel: "stylesheet", href: ICONS_CSS }
+        document::Link { rel: "stylesheet", href: MODAL_CSS }
         Router::<Route> {}
     }
 }
--- src/top_page.rs
+++ src/top_page.rs
@@ -16,12 +16,14 @@ struct Series {
 const FAVOURITE_ICON: Asset = asset!("assets/icons/bookmark.svg");
 const EDIT_ICON: Asset = asset!("assets/icons/edit.svg");
 const READ_ICON: Asset = asset!("assets/icons/read.svg");
+const DELETE_ICON: Asset = asset!("assets/icons/delete.svg");
 
 #[component]
 fn DrawSeries(
     series: Series,
     on_favorite_click: EventHandler<MouseEvent>,
     on_click: EventHandler<MouseEvent>,
+    on_delete_click: EventHandler<MouseEvent>,
 ) -> Element {
     let fav_icon_style = if series.is_favourite {
         "background-color: #ffa0b0ff;"
@@ -47,9 +49,24 @@ fn DrawSeries(
                         on_favorite_click.call(evt);
                     },
                 }
+                div {
+                    class: "action_icon delete",
+                    style: "mask-image: url({DELETE_ICON}); -webkit-mask-image: url({DELETE_ICON});",
+                    onclick: move |evt| {
+                        evt.stop_propagation();
+                        on_delete_click.call(evt);
+                    },
+                }
             }
         }
     }
+}
+
+#[derive(Clone, PartialEq)]
+enum DeleteTarget {
+    None,
+    Series(usize),
+    Chapter(usize, usize),
 }
 
 #[derive(Clone, PartialEq)]
@@ -83,6 +100,7 @@ pub fn Top() -> Element {
     });
 
     let mut panel_state = use_signal(|| PanelState::None);
+    let mut delete_target = use_signal(|| DeleteTarget::None);
     let mut new_series_title = use_signal(|| String::new());
     let mut new_chapter_title = use_signal(|| String::new());
 
@@ -100,6 +118,9 @@ pub fn Top() -> Element {
                         },
                         on_click: move |_| {
                             panel_state.set(PanelState::Selected(i));
+                        },
+                        on_delete_click: move |_| {
+                            delete_target.set(DeleteTarget::Series(i));
                         }
                     }
                 }
@@ -119,7 +140,7 @@ pub fn Top() -> Element {
                     PanelState::Selected(index) => rsx! {
                         h2 { "{series.read()[index].title}" }
                         ul {
-                            for chapter in &series.read()[index].chapters {
+                            for (chapter_idx, chapter) in series.read()[index].chapters.clone().into_iter().enumerate() {
                                 li {
                                     div {
                                         "{chapter.title}"
@@ -135,6 +156,13 @@ pub fn Top() -> Element {
                                         div {
                                             class: "action_icon",
                                             style: "mask-image: url({READ_ICON}); -webkit-mask-image: url({READ_ICON});"
+                                        }
+                                        div {
+                                            class: "action_icon delete",
+                                            style: "mask-image: url({DELETE_ICON}); -webkit-mask-image: url({DELETE_ICON});",
+                                            onclick: move |_| {
+                                                delete_target.set(DeleteTarget::Chapter(index, chapter_idx));
+                                            }
                                         }
                                     }
                                 }
@@ -217,6 +245,73 @@ pub fn Top() -> Element {
                     PanelState::None => rsx! {
                         p { "シリーズが選択されていません" }
                     },
+                }
+            }
+        }
+        if let DeleteTarget::Series(i) = delete_target() {
+            div {
+                class: "modal_overlay",
+                div {
+                    class: "modal_content",
+                    p { "本当に「{series.read()[i].title}」を削除しますか？" }
+                    div {
+                        class: "form_actions",
+                        style: "justify-content: center; gap: 20px; margin-top: 20px;",
+                        button {
+                            class: "danger",
+                            onclick: move |_| {
+                                series.write().remove(i);
+                                match panel_state() {
+                                    PanelState::Selected(selected_index) => {
+                                        if selected_index == i {
+                                            panel_state.set(PanelState::None);
+                                        } else if selected_index > i {
+                                            panel_state.set(PanelState::Selected(selected_index - 1));
+                                        }
+                                    }
+                                    PanelState::CreateChapter(selected_index) => {
+                                        if selected_index == i {
+                                            panel_state.set(PanelState::None);
+                                        } else if selected_index > i {
+                                            panel_state.set(PanelState::CreateChapter(selected_index - 1));
+                                        }
+                                    }
+                                    _ => {}
+                                }
+                                delete_target.set(DeleteTarget::None);
+                            },
+                            "はい"
+                        }
+                        button {
+                            onclick: move |_| delete_target.set(DeleteTarget::None),
+                            "いいえ"
+                        }
+                    }
+                }
+            }
+        }
+        if let DeleteTarget::Chapter(series_idx, chapter_idx) = delete_target() {
+             div {
+                class: "modal_overlay",
+                div {
+                    class: "modal_content",
+                    p { "本当に「{series.read()[series_idx].chapters[chapter_idx].title}」を削除しますか？" }
+                    div {
+                        class: "form_actions",
+                        style: "justify-content: center; gap: 20px; margin-top: 20px;",
+                        button {
+                            class: "danger",
+                            onclick: move |_| {
+                                series.write()[series_idx].chapters.remove(chapter_idx);
+                                delete_target.set(DeleteTarget::None);
+                            },
+                            "はい"
+                        }
+                        button {
+                            onclick: move |_| delete_target.set(DeleteTarget::None),
+                            "いいえ"
+                        }
+                    }
                 }
             }
         }
