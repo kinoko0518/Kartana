e5637964626120d6455b3d7cf0c8bce003bd00fd
6osciola <99010199+kinoko0518@users.noreply.github.com>
2025-12-30 17:28:35 +0900

    I refactored top_pages

--- src/top_page.rs
+++ src/top_page.rs
@@ -1,5 +1,10 @@
 use dioxus::prelude::*;
 
+const FAVOURITE_ICON: Asset = asset!("assets/icons/bookmark.svg");
+const EDIT_ICON: Asset = asset!("assets/icons/edit.svg");
+const READ_ICON: Asset = asset!("assets/icons/read.svg");
+const DELETE_ICON: Asset = asset!("assets/icons/delete.svg");
+
 #[derive(PartialEq, Eq, Clone)]
 struct Chapter {
     title: String,
@@ -11,55 +16,6 @@ struct Series {
     title: String,
     is_favourite: bool,
     chapters: Vec<Chapter>,
-}
-
-const FAVOURITE_ICON: Asset = asset!("assets/icons/bookmark.svg");
-const EDIT_ICON: Asset = asset!("assets/icons/edit.svg");
-const READ_ICON: Asset = asset!("assets/icons/read.svg");
-const DELETE_ICON: Asset = asset!("assets/icons/delete.svg");
-
-#[component]
-fn DrawSeries(
-    series: Series,
-    on_favorite_click: EventHandler<MouseEvent>,
-    on_click: EventHandler<MouseEvent>,
-    on_delete_click: EventHandler<MouseEvent>,
-) -> Element {
-    let fav_icon_style = if series.is_favourite {
-        "background-color: #ffa0b0ff;"
-    } else {
-        "background-color: white;"
-    };
-
-    rsx! {
-        div {
-            class: "series_container",
-            onclick: move |evt| on_click.call(evt),
-            p {
-                class: "series_title",
-                "{series.title}"
-            }
-            div {
-                class: "series_actions",
-                div {
-                    class: "action_icon",
-                    style: "{fav_icon_style} mask-image: url({FAVOURITE_ICON}); -webkit-mask-image: url({FAVOURITE_ICON});",
-                    onclick: move |evt| {
-                        evt.stop_propagation();
-                        on_favorite_click.call(evt);
-                    },
-                }
-                div {
-                    class: "action_icon delete",
-                    style: "mask-image: url({DELETE_ICON}); -webkit-mask-image: url({DELETE_ICON});",
-                    onclick: move |evt| {
-                        evt.stop_propagation();
-                        on_delete_click.call(evt);
-                    },
-                }
-            }
-        }
-    }
 }
 
 #[derive(Clone, PartialEq)]
@@ -75,6 +31,125 @@ enum PanelState {
     Selected(usize),
     CreateSeries,
     CreateChapter(usize),
+}
+
+#[component]
+fn ActionIcon(
+    icon: Asset,
+    onclick: EventHandler<MouseEvent>,
+    class: Option<String>,
+    style: Option<String>,
+) -> Element {
+    let extra_class = class.unwrap_or_default();
+    let extra_style = style.unwrap_or_default();
+    
+    rsx! {
+        div {
+            class: "action_icon {extra_class}",
+            style: "mask-image: url({icon}); -webkit-mask-image: url({icon}); {extra_style}",
+            onclick: move |evt| {
+                evt.stop_propagation();
+                onclick.call(evt);
+            },
+        }
+    }
+}
+
+#[component]
+fn CreateForm(
+    title_header: String,
+    placeholder: String,
+    value: String,
+    oninput: EventHandler<String>,
+    oncreate: EventHandler<MouseEvent>,
+    oncancel: EventHandler<MouseEvent>,
+) -> Element {
+    rsx! {
+        div {
+            class: "create_form",
+            h2 { "{title_header}" }
+            input {
+                value: "{value}",
+                oninput: move |evt| oninput.call(evt.value()),
+                placeholder: "{placeholder}",
+            }
+            div {
+                class: "form_actions",
+                button {
+                    onclick: move |evt| oncreate.call(evt),
+                    "作成"
+                }
+                button {
+                    onclick: move |evt| oncancel.call(evt),
+                    "キャンセル"
+                }
+            }
+        }
+    }
+}
+
+#[component]
+fn ConfirmationModal(
+    message: String,
+    onconfirm: EventHandler<MouseEvent>,
+    oncancel: EventHandler<MouseEvent>,
+) -> Element {
+    rsx! {
+        div {
+            class: "modal_overlay",
+            div {
+                class: "modal_content",
+                p { "{message}" }
+                div {
+                    class: "form_actions",
+                    style: "justify-content: center; gap: 20px; margin-top: 20px;",
+                    button {
+                        class: "danger",
+                        onclick: move |evt| onconfirm.call(evt),
+                        "はい"
+                    }
+                    button {
+                        onclick: move |evt| oncancel.call(evt),
+                        "いいえ"
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[component]
+fn DrawSeries(
+    series: Series,
+    on_favorite_click: EventHandler<MouseEvent>,
+    on_click: EventHandler<MouseEvent>,
+    on_delete_click: EventHandler<MouseEvent>,
+) -> Element {
+    let fav_bg_color = if series.is_favourite { "#ffa0b0ff" } else { "white" };
+
+    rsx! {
+        div {
+            class: "series_container",
+            onclick: move |evt| on_click.call(evt),
+            p {
+                class: "series_title",
+                "{series.title}"
+            }
+            div {
+                class: "series_actions",
+                ActionIcon {
+                    icon: FAVOURITE_ICON,
+                    style: "background-color: {fav_bg_color};",
+                    onclick: move |evt| on_favorite_click.call(evt),
+                }
+                ActionIcon {
+                    icon: DELETE_ICON,
+                    class: "delete",
+                    onclick: move |evt| on_delete_click.call(evt),
+                }
+            }
+        }
+    }
 }
 
 #[component]
@@ -149,17 +224,17 @@ pub fn Top() -> Element {
                                     }
                                     div {
                                         class: "chapter_actions",
-                                        div {
-                                            class: "action_icon",
-                                            style: "mask-image: url({EDIT_ICON}); -webkit-mask-image: url({EDIT_ICON});"
+                                        ActionIcon {
+                                            icon: EDIT_ICON,
+                                            onclick: |_| {}, // Placeholder
                                         }
-                                        div {
-                                            class: "action_icon",
-                                            style: "mask-image: url({READ_ICON}); -webkit-mask-image: url({READ_ICON});"
+                                        ActionIcon {
+                                            icon: READ_ICON,
+                                            onclick: |_| {}, // Placeholder
                                         }
-                                        div {
-                                            class: "action_icon delete",
-                                            style: "mask-image: url({DELETE_ICON}); -webkit-mask-image: url({DELETE_ICON});",
+                                        ActionIcon {
+                                            icon: DELETE_ICON,
+                                            class: "delete",
                                             onclick: move |_| {
                                                 delete_target.set(DeleteTarget::Chapter(index, chapter_idx));
                                             }
@@ -181,65 +256,41 @@ pub fn Top() -> Element {
                         }
                     },
                     PanelState::CreateSeries => rsx! {
-                        div {
-                            class: "create_form",
-                            h2 { "新しいシリーズを作成" }
-                            input {
-                                value: "{new_series_title}",
-                                oninput: move |evt| new_series_title.set(evt.value()),
-                                placeholder: "シリーズタイトル",
-                            }
-                            div {
-                                class: "form_actions",
-                                button {
-                                    onclick: move |_| {
-                                        if !new_series_title().trim().is_empty() {
-                                            series.write().push(Series {
-                                                title: new_series_title(),
-                                                is_favourite: false,
-                                                chapters: vec![],
-                                            });
-                                            let new_index = series.read().len() - 1;
-                                            panel_state.set(PanelState::Selected(new_index));
-                                        }
-                                    },
-                                    "作成"
+                        CreateForm {
+                            title_header: "新しいシリーズを作成",
+                            placeholder: "シリーズタイトル",
+                            value: "{new_series_title}",
+                            oninput: move |val: String| new_series_title.set(val),
+                            oncreate: move |_| {
+                                if !new_series_title().trim().is_empty() {
+                                    series.write().push(Series {
+                                        title: new_series_title(),
+                                        is_favourite: false,
+                                        chapters: vec![],
+                                    });
+                                    let new_index = series.read().len() - 1;
+                                    panel_state.set(PanelState::Selected(new_index));
                                 }
-                                button {
-                                    onclick: move |_| panel_state.set(PanelState::None),
-                                    "キャンセル"
-                                }
-                            }
+                            },
+                            oncancel: move |_| panel_state.set(PanelState::None),
                         }
                     },
                     PanelState::CreateChapter(index) => rsx! {
-                        div {
-                            class: "create_form",
-                            h2 { "新しいチャプターを作成" }
-                             input {
-                                value: "{new_chapter_title}",
-                                oninput: move |evt| new_chapter_title.set(evt.value()),
-                                placeholder: "チャプタータイトル",
-                            }
-                            div {
-                                class: "form_actions",
-                                button {
-                                    onclick: move |_| {
-                                        if !new_chapter_title().trim().is_empty() {
-                                            series.write()[index].chapters.push(Chapter {
-                                                title: new_chapter_title(),
-                                                created_at: "2025-01-01".into(), // Default date for now
-                                            });
-                                            panel_state.set(PanelState::Selected(index));
-                                        }
-                                    },
-                                    "作成"
+                        CreateForm {
+                            title_header: "新しいチャプターを作成",
+                            placeholder: "チャプタータイトル",
+                            value: "{new_chapter_title}",
+                            oninput: move |val: String| new_chapter_title.set(val),
+                            oncreate: move |_| {
+                                if !new_chapter_title().trim().is_empty() {
+                                    series.write()[index].chapters.push(Chapter {
+                                        title: new_chapter_title(),
+                                        created_at: "2025-01-01".into(),
+                                    });
+                                    panel_state.set(PanelState::Selected(index));
                                 }
-                                button {
-                                    onclick: move |_| panel_state.set(PanelState::Selected(index)),
-                                    "キャンセル"
-                                }
-                            }
+                            },
+                            oncancel: move |_| panel_state.set(PanelState::Selected(index)),
                         }
                     },
                     PanelState::None => rsx! {
@@ -248,72 +299,45 @@ pub fn Top() -> Element {
                 }
             }
         }
-        if let DeleteTarget::Series(i) = delete_target() {
-            div {
-                class: "modal_overlay",
-                div {
-                    class: "modal_content",
-                    p { "本当に「{series.read()[i].title}」を削除しますか？" }
-                    div {
-                        class: "form_actions",
-                        style: "justify-content: center; gap: 20px; margin-top: 20px;",
-                        button {
-                            class: "danger",
-                            onclick: move |_| {
-                                series.write().remove(i);
-                                match panel_state() {
-                                    PanelState::Selected(selected_index) => {
-                                        if selected_index == i {
-                                            panel_state.set(PanelState::None);
-                                        } else if selected_index > i {
-                                            panel_state.set(PanelState::Selected(selected_index - 1));
-                                        }
+        
+        match delete_target() {
+            DeleteTarget::Series(i) => rsx! {
+                ConfirmationModal {
+                    message: format!("本当に「{}」を削除しますか？", series.read()[i].title),
+                    onconfirm: move |_| {
+                        series.write().remove(i);
+                        match panel_state() {
+                            PanelState::Selected(selected_index) | PanelState::CreateChapter(selected_index) => {
+                                if selected_index == i {
+                                    panel_state.set(PanelState::None);
+                                } else if selected_index > i {
+                                    // If we are selecting a series after the one being deleted, its index shifts down by 1
+                                    // We need to preserve the mode (Selected or CreateChapter)
+                                    match panel_state() {
+                                        PanelState::Selected(_) => panel_state.set(PanelState::Selected(selected_index - 1)),
+                                        PanelState::CreateChapter(_) => panel_state.set(PanelState::CreateChapter(selected_index - 1)),
+                                        _ => {}
                                     }
-                                    PanelState::CreateChapter(selected_index) => {
-                                        if selected_index == i {
-                                            panel_state.set(PanelState::None);
-                                        } else if selected_index > i {
-                                            panel_state.set(PanelState::CreateChapter(selected_index - 1));
-                                        }
-                                    }
-                                    _ => {}
                                 }
-                                delete_target.set(DeleteTarget::None);
-                            },
-                            "はい"
+                            }
+                            _ => {}
                         }
-                        button {
-                            onclick: move |_| delete_target.set(DeleteTarget::None),
-                            "いいえ"
-                        }
-                    }
+                        delete_target.set(DeleteTarget::None);
+                    },
+                    oncancel: move |_| delete_target.set(DeleteTarget::None),
                 }
-            }
-        }
-        if let DeleteTarget::Chapter(series_idx, chapter_idx) = delete_target() {
-             div {
-                class: "modal_overlay",
-                div {
-                    class: "modal_content",
-                    p { "本当に「{series.read()[series_idx].chapters[chapter_idx].title}」を削除しますか？" }
-                    div {
-                        class: "form_actions",
-                        style: "justify-content: center; gap: 20px; margin-top: 20px;",
-                        button {
-                            class: "danger",
-                            onclick: move |_| {
-                                series.write()[series_idx].chapters.remove(chapter_idx);
-                                delete_target.set(DeleteTarget::None);
-                            },
-                            "はい"
-                        }
-                        button {
-                            onclick: move |_| delete_target.set(DeleteTarget::None),
-                            "いいえ"
-                        }
-                    }
+            },
+            DeleteTarget::Chapter(series_idx, chapter_idx) => rsx! {
+                ConfirmationModal {
+                    message: format!("本当に「{}」を削除しますか？", series.read()[series_idx].chapters[chapter_idx].title),
+                    onconfirm: move |_| {
+                        series.write()[series_idx].chapters.remove(chapter_idx);
+                        delete_target.set(DeleteTarget::None);
+                    },
+                    oncancel: move |_| delete_target.set(DeleteTarget::None),
                 }
-            }
+            },
+            DeleteTarget::None => rsx! {},
         }
     }
 }
